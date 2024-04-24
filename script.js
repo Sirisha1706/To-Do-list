b765bfd09f9e4893a943da1749a303323b6d0aca
Sirisha1706 <sirishasundharisadhu@gmail.com>
2024-04-24 09:53:01 +0530

    .

---
+++ script.js
@@ -0,0 +1,47 @@
+var listEle = ['Learn ReactJs', 'Create Projects'];
+
+const taskBtn = document.getElementById('btn');
+const eleList = document.querySelector('#todolist');
+const tasksCont = document.querySelector('.footer');
+
+listEle.forEach((ele, index) => element(ele, index));
+
+function element(ele, key){
+    const list = document.createElement('li');
+    list.textContent = ele;
+    const buttn = document.createElement('button');
+    buttn.innerHTML = `<i class="fa-solid fa-circle-xmark fa-xl"></i>`
+    buttn.accessKey = key;
+    buttn.classList.add('btn');
+    buttn.addEventListener('click', ()=> removeEle(list) );
+    list.appendChild(buttn);
+    eleList.appendChild(list);
+};
+
+function renderList(){
+    const val = document.getElementById('input');
+    if(val.value === '')
+        alert('Please! Enter the task');
+    else{
+        listEle.push(val.value);
+        element(val.value);
+        val.value='';
+        updateTasksC();
+    }
+};
+
+taskBtn.addEventListener('click', renderList);
+
+function removeEle(ele){
+    listEle = listEle.filter(val => val != ele.textContent);
+    console.log(listEle);
+    ele.remove();
+    updateTasksC();
+}
+
+function updateTasksC(){
+    const noOfTasks = document.createElement('label');
+    noOfTasks.textContent = `${listEle.length} tasks left.`;
+    tasksCont.insertBefore(noOfTasks, tasksCont.firstChild);
+}
+updateTasksC();
